1a52cba13d55c4c32f3bef85abcc44ac3ee7fd45
umar khan <umar.khan@student.uts.edu.au>
2023-01-14 00:22:43 +1100

    progress

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import "./App.css";
+const App = () => {
+  const activeMenu = true;
+  return (
+    <>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="settings" position="Top">
+              <button type="button">
+                <FiSettings
+                  className="text-6xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                  style={{ background: "blue", borderRadius: "50%" }}
+                />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg"> Sidebar w-0 </div>
+          )}
+          <div
+            className={
+              activeMenu
+                ? "dark:bg-main-bg bg-main-bg min-h-screen md:ml-72w-full"
+                : "dark:bg-main-bg bg-main-bg min-h-screen w-full flex-2"
+            }
+          ></div>
+        </div>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
